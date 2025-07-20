2b71e4dc7afdea97140a5c0cb978c149f5f5d624
upii26 <balefighet@gmail.com>
2025-07-20 14:28:01 +0700

    update2

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -5,11 +5,11 @@ import nationalG from './../assets/images/nationalG.png'
 import pixar from './../assets/images/pixar.png'
 import starwar from './../assets/images/starwar.png'
  
-import starwarV from './../assets/Videos/star-wars.mp4'
-import disneyV from './../assets/Videos/disney.mp4'
-import marvelV from './../assets/Videos/marvel.mp4'
-import nationalGeographicV from './../assets/Videos/national-geographic.mp4'
-import pixarV from './../assets/Videos/pixar.mp4'
+import starwarV from './../assets/videos/star-wars.mp4'
+import disneyV from './../assets/videos/disney.mp4'
+import marvelV from './../assets/videos/marvel.mp4'
+import nationalGeographicV from './../assets/videos/national-geographic.mp4'
+import pixarV from './../assets/videos/pixar.mp4'
 
 const ProductionHouse = () => {
 
